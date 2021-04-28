07af31a22578a0a26125454739b4a9847ad08ef5
Richard <richard.yang.tong@gmail.com>
2021-04-28 19:46:21 -0400

    Added a expression with math operation.

--- src/Hello.java
+++ src/Hello.java
@@ -2,7 +2,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World");
 
-        int myFirstNumber = 5;
+        int myFirstNumber = 10 + 5;
         System.out.println(myFirstNumber);
     }
 }
